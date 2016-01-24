2d3d0d5d82c9a6b765030a1da4797f981216ecb0
Evin Ugur <evinoog96@gmail.com>
2016-01-24 01:30:27 -0500

    .

--- player.js
+++ player.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
 	window.url = location.search.substring("?url=".length);	
+	var videoElement = $("#vidEl");
+	videoElement.attr('src', url);
+	videoElement[0].load();
 });
