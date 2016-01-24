a68b4dedf4803f4fa09976c8fba5a22bffc7412b
Evin Ugur <evinoog96@gmail.com>
2016-01-24 01:44:40 -0500

    whoops

---
+++ content.js
@@ -0,0 +1,17 @@
+var messages = [];
+chrome.runtime.sendMessage("init"); 
+chrome.runtime.sendMessage({
+		from : 'content',
+		subject : 'showPageAction',
+});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+	// this is when you get a message from the backgrounsd
+	if (request.from === "request_listener" && request.subject === "contentFound")
+		messages.push(request.payload);
+});
+// listen for messages from the popup view 
+chrome.runtime.onMessage.addListener(function(msg, sender, resp) {
+	if (msg.from === 'popup' && msg.subject === 'DOMInfo') {
+		resp(messages);
+	}
+});
