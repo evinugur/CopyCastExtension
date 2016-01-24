5ed4fd90d4ca5c7cf174d974d5bd89ec38ac187e
Evin Ugur <evinoog96@gmail.com>
2016-01-24 06:06:36 -0500

    load custom resource feature

--- player.js
+++ player.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
 	var videoElement = $("#vidEl");
 	videoElement.attr('src', url);
 	videoElement[0].load();
+	$("#urlInputBtn").click(function() {
+		var url = $("#urlInputBox").val();
+		var arg = "?url=";
+		var argIndex = location.href.indexOf(arg) + arg.length;
+		location.href = location.href.substring(0, argIndex) + url;
+	});
 });
